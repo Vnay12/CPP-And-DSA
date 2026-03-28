c3c3b7754f893da5ba526404656d1e433295ffb3
Vnay12 <vnay1712@gmail.com>
2026-03-28 19:12:28 +0530

    DSA

---
+++ Data Structure/Stack/StackSLL.cpp
@@ -0,0 +1,24 @@
+/*
+This code is stack implementation using LinkedList    ////// NOT DONE YET ///////////
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+}
